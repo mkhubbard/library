076ad64a94b475e76c8e5214cb6e9443da891ade
Michael Hubbard <mkhubbard@gmail.com>
2014-09-09 22:24:52 -0400

    Fixed backbone.js dependency, custom asset installtion is now performed automatically by composer.

---
+++ src/Library/Bundle/AppBundle/Composer/ScriptHandler.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Library\Bundle\AppBundle\Composer;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Composer\Script\CommandEvent;
+
+/**
+ * Most of this was lifted directly from the Symfony framework.
+ */
+class ScriptHandler
+{
+    /**
+     * Composer variables are declared static so that an event could update
+     * a composer.json and set new options, making them immediately available
+     * to forthcoming listeners.
+     */
+    private static $options = array(
+        'symfony-app-dir' => 'app',
+        'symfony-web-dir' => 'web'
+    );
+    /**
+     * Installs the assets under the web root directory.
+     *
+     * For better interoperability, assets are copied instead of symlinked by default.
+     *
+     * Even if symlinks work on Windows, this is only true on Windows Vista and later,
+     * but then, only when running the console with admin rights or when disabling the
+     * strict user permission checks (which can be done on Windows 7 but not on Windows
+     * Vista).
+     *
+     * @param $event CommandEvent A instance
+     */
+    public static function installAssets(CommandEvent $event)
+    {
+        $options = self::getOptions($event);
+        $consoleDir = self::getConsoleDir($event, 'install assets');
+
+        if (null === $consoleDir) {
+            return;
+        }
+
+        $webDir = $options['symfony-web-dir'];
+
+        if (!self::hasDirectory($event, 'symfony-web-dir', $webDir, 'install assets')) {
+            return;
+        }
+
+        static::executeCommand($event, $consoleDir, 'library:assets:integrate');
+    }
+
+    protected static function hasDirectory(CommandEvent $event, $configName, $path, $actionName)
+    {
+        if (!is_dir($path)) {
+            $event->getIO()->write(sprintf('The %s (%s) specified in composer.json was not found in %s, can not %s.', $configName, $path, getcwd(), $actionName));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    protected static function executeCommand(CommandEvent $event, $consoleDir, $cmd, $timeout = 300)
+    {
+        $php = escapeshellarg(self::getPhp());
+        $console = escapeshellarg($consoleDir.'/console');
+        if ($event->getIO()->isDecorated()) {
+            $console .= ' --ansi';
+        }
+
+        $process = new Process($php.' '.$console.' '.$cmd, null, null, null, $timeout);
+        $process->run(function ($type, $buffer) use ($event) { $event->getIO()->write($buffer, false); });
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
+        }
+    }
+
+    /**
+     * Returns a relative path to the directory that contains the `console` command.
+     *
+     * @param CommandEvent $event      The command event.
+     * @param string       $actionName The name of the action
+     *
+     * @return string|null The path to the console directory, null if not found.
+     */
+    protected static function getConsoleDir(CommandEvent $event, $actionName)
+    {
+        $options = self::getOptions($event);
+
+        if (self::useNewDirectoryStructure($options)) {
+            if (!self::hasDirectory($event, 'symfony-bin-dir', $options['symfony-bin-dir'], $actionName)) {
+                return;
+            }
+
+            return $options['symfony-bin-dir'];
+        }
+
+        if (!self::hasDirectory($event, 'symfony-app-dir', $options['symfony-app-dir'], 'execute command')) {
+            return;
+        }
+
+        return $options['symfony-app-dir'];
+    }
+
+    /**
+     * Returns true if the new directory structure is used.
+     *
+     * @param array $options Composer options
+     *
+     * @return bool
+     */
+    protected static function useNewDirectoryStructure(array $options)
+    {
+        return isset($options['symfony-var-dir']) && is_dir($options['symfony-var-dir']);
+    }
+
+    protected static function getOptions(CommandEvent $event)
+    {
+        $options = array_merge(self::$options, $event->getComposer()->getPackage()->getExtra());
+
+        return $options;
+    }
+
+    protected static function getPhp()
+    {
+        $phpFinder = new PhpExecutableFinder;
+        if (!$phpPath = $phpFinder->find()) {
+            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        return $phpPath;
+    }
+}
